1c0e00d316186a382cca3055350346c10902d6cd
GustavoMont <lgmluisgm@gmail.com>
2025-11-21 13:31:07 -0300

    style: config `eslint`

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginJest from "eslint-plugin-jest";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  pluginJest.configs["flat/recommended"],
+]);
+
+export default eslintConfig;
