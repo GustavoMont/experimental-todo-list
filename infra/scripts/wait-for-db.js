176498ecc0b97f91c7751fdc57d3251ed6d5ebe3
GustavoMont <lgmluisgm@gmail.com>
2025-11-22 09:09:08 -0300

    build: add `wait-for-db` script

---
+++ infra/scripts/wait-for-db.js
@@ -0,0 +1,19 @@
+import { exec } from "node:child_process";
+
+function checkDb() {
+  exec(
+    "docker exec experimental_todo_postgres pg_isready --host localhost",
+    handleReturn
+  );
+
+  function handleReturn(err, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkDb();
+      return;
+    }
+    console.log("\n🎉 Postgres está pronto e aceitando conexões 🎉\n");
+  }
+}
+process.stdout.write("🔴 Aguardando Postgres aceitar conexões");
+checkDb();
